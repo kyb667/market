836e74acf09bb1d3cf2c46a5605c8b52c7fd44c0
ebkwon <kyb667@naver.com>
2023-06-18 22:24:13 +0900

    [ ADD ] fe port 8080 -> 8000

--- fe/marketFe/market-fe/vue.config.js
+++ fe/marketFe/market-fe/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8000,
+    proxy: {
+      '^/context/api': {
+        target: 'http://host.docker.internal:8080',
+        pathRewrite: { '^/context/api': '' },
+        changeOrigin: true,
+        logLevel: 'debug',
+      },
+    },
+  },
 })
